d3abf21896e2cc6f514c8e2efd493c06026c5731
salmanrf <salmanrf2@gmail.com>
2025-11-30 09:21:28 +0000

    fix: Use connection pool, and transaction for delete org API (#5)

--- go.mod
+++ go.mod
@@ -5,6 +5,11 @@ go 1.24.2
 require github.com/jackc/pgx/v5 v5.7.6
 
 require (
+	github.com/jackc/puddle/v2 v2.2.2 // indirect
+	golang.org/x/sync v0.13.0 // indirect
+)
+
+require (
 	github.com/alexedwards/argon2id v1.0.0
 	github.com/golang-jwt/jwt/v5 v5.3.0
 	github.com/jackc/pgpassfile v1.0.0 // indirect
--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
 	"github.com/salmanrf/capybara-cloud/api"
 	"github.com/salmanrf/capybara-cloud/internal/auth"
@@ -17,35 +17,44 @@ import (
 	auth_utils "github.com/salmanrf/capybara-cloud/pkg/auth"
 )
 
-func setup() (context.Context, *pgx.Conn, error) {
+func create_db_conn(ctx context.Context, db_uri string) *pgxpool.Pool {
+	dbpool, err := pgxpool.New(ctx, db_uri)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to create connection pool: %v\n", err)
+		os.Exit(1)
+	}
+
+	return dbpool
+}
+
+func setup() (context.Context, *pgxpool.Pool, error) {
 	ctx := context.Background()
 
 	err := godotenv.Load()
 	if err != nil {
-		return ctx, nil, err
+		fmt.Println("Unable to load env vars")
+		os.Exit(1)
 	}
 
 	postgres_uri := os.Getenv("POSTGRES_URI")
+	dbpool := create_db_conn(ctx, postgres_uri)
 
-	conn, err := pgx.Connect(ctx, postgres_uri)
-	if err != nil {
-		return ctx, nil, err
-	}
-
-	err = conn.Ping(ctx)
+	err = dbpool.Ping(ctx)
 
 	if err != nil {
-		return ctx, nil, err
+		fmt.Println("Unable to ping database")
+		os.Exit(1)
 	}
 
 	fmt.Println("Database connection established")
 
-	return ctx, conn, nil
+	return ctx, dbpool, nil
 }
 
 func main() {
 	ctx, db_conn, err := setup()
-	defer db_conn.Close(ctx)
+	defer db_conn.Close()
 	defer func() {
 		fmt.Println("Server is stopped")
 	}()
@@ -56,7 +65,7 @@ func main() {
 	queries := database.New(db_conn)
 	user_service := user.NewService(ctx, queries)
 	auth_service := auth.NewService(ctx, user_service)
-	org_service := organization.NewService(ctx, queries, user_service)
+	org_service := organization.NewService(ctx, db_conn, queries, user_service)
 	jwt_utils := auth_utils.NewJWTUtils(os.Getenv("AUTH_JWT_SECRET"))
 	
 	api_server := api.NewAPIServer(
